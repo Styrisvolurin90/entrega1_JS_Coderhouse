509352e25c40ea8019aef572365f547fa20a2355
Hiram Peña Franco <hapf.58@gmail.com>
2023-12-25 17:29:52 -0600

    entrega 1 JS Coderhouse

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const Tienda = "Tienda Conquer"
+
+let oversized = 450
+let recto = 250
+let slimFit = 300
+let mujer = 250
+
+let nombre = prompt("Bienvenido a tienda Conquer. Ingresa tu nombre:")
+
+console.log("Bienvenido:", nombre)
+
+alert("Elige la prenda que quieras llevar" + nombre)
+
