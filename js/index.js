f0779b9edb2dd99d9ce5446d10476941b2134567
Hiram Peña Franco <hapf.58@gmail.com>
2023-12-26 00:58:21 -0600

    proyecto

--- js/index.js
+++ js/index.js
@@ -5,9 +5,54 @@ let recto = 250
 let slimFit = 300
 let mujer = 250
 
+
 let nombre = prompt("Bienvenido a tienda Conquer. Ingresa tu nombre:")
 
 console.log("Bienvenido:", nombre)
 
-alert("Elige la prenda que quieras llevar" + nombre)
+function mostrarPlayera(codigo) {
+    switch (codigo) {
+        case 1:
+            alert(nombre + ", " + "Has elegido la prenda Oversized")
+            //Mostrar tipo de prenda//
+            break
+        case 2:
+            alert(nombre + ", " + "Has elegido la prenda de Corte Recto")
+            //Mostrar tipo de prenda//
+            break
+        case 3:
+            alert(nombre + ", " + "Has elegido la prenda Slim Fit")
+            //Mostrar tipo de prenda//
+            break
+        case 4:
+            alert(nombre + ", " + "Has elegido la prenda para Mujer")
+            //Mostrar tipo de prenda//
+            break
+        default:
+            alert("No has elegido correctamente. Intenta de nuevo")
+    }
+}
 
+function mostrarPrecio(codigo) {
+    switch (codigo) {
+        case 1:
+            alert(nombre + ", " + "tiene un precio de $450") 
+        case 2:
+            alert(nombre + ", " + "tiene un precio de $250")
+        case 3:
+            alert(nombre + ", " + "tiene un precio de $300")
+        case 4:
+            alert(nombre + ", " + "tiene un precio de $250")
+    }
+
+
+}
+
+function consultarPlayeras() {
+    let codigoPlayera = prompt(nombre + ", " + "Elige la prenda que quieras llevar")
+    if (codigoPlayera !== "" && codigoPlayera !== null) {
+        mostrarPrecio(parseInt(codigoPlayera))
+    } else {
+        console.alert("Ingresa una prenda válida.")
+    }
+}
